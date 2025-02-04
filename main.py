5290ee0f891560e0b181c11f770817bb96fed738
Siddh8051 <siddharthkumar805100@gmail.com>
2025-02-04 21:39:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -168,32 +168,7 @@ async def upload(bot: Client, m: Message):
             name1 = links[i][0].replace("\t", "").replace(":", "").replace("/", "").replace("+", "").replace("#", "").replace("|", "").replace("@", "").replace("*", "").replace(".", "").replace("https", "").replace("http", "").strip()
             name = f'{str(count).zfill(3)}) {name1[:60]}'
 
-	  #  if "encrypted" in url:
-          #   decrypted_video = await download_and_decrypt_video(url, name, key)
-           # if decrypted_video:
-           # await send_vid(bot, m, "Video Decrypted", decrypted_video, "no", name, prog)
-          #  else:
-          #  await m.reply_text(f"Decryption failed for {name}")
-        #    else:
-    # Standard video download logic
-          #  await download_video(url, cmd, name)
-
-          # Integration into download process
-          #  async def download_and_decrypt_video(url, name, decryption_key):
-    # Step 1: Download the encrypted file
-          #  encrypted_file_path = await download(url, name)
-	
-    # Step 2: Decrypt the file using the provided key
-          #  decryption_success = decrypt_file(encrypted_file_path, decryption_key = key)
-
-          #  if decryption_success:
-               #  print(f"Decryption successful for {name}")
-              #   return encrypted_file_path
-          #  else:
-               #  print(f"Decryption failed for {name}")
-               #  return None
-	
-
+	  
 
 
             if "youtu" in url:
